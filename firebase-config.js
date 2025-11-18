dd71cc3ec04cdc85b706b0f5673c701f5cbdfdff
Sergei Voron`ko <60227199+Noncence@users.noreply.github.com>
2025-11-18 18:29:35 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-import { initializeApp } from 'firebase/app';
+  const firebaseConfig = {
+    authDomain: "kid-tasks-95211.firebaseapp.com",
+    projectId: "kid-tasks-95211",
+    storageBucket: "kid-tasks-95211.firebasestorage.app",
+    messagingSenderId: "1039260161726",
+    appId: "1:1039260161726:web:e4e87489dcc0dbe38a30ef"
+  };
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-};
-
-const app = initializeApp(firebaseConfig);
-export default app;
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
