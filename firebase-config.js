cc6d51c61d6a18bdf3e166ba24780fd8293d88df
Sergei Voron`ko <60227199+Noncence@users.noreply.github.com>
2025-11-18 23:47:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,1 @@
-  const firebaseConfig = {
-    authDomain: "kid-tasks-95211.firebaseapp.com",
-    projectId: "kid-tasks-95211",
-    storageBucket: "kid-tasks-95211.firebasestorage.app",
-    messagingSenderId: "1039260161726",
-    appId: "1:1039260161726:web:e4e87489dcc0dbe38a30ef"
-  };
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
