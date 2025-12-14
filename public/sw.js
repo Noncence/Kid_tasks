56a850463bd5f25abac7b7f1663066611eb082df
Sergei Voron`ko <60227199+Noncence@users.noreply.github.com>
2025-12-14 17:26:00 +0300

    Delete public directory

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-// sw.js
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('child-tasks').then(cache => 
-            cache.addAll(['/', '/styles.css', '/script.js'])
-        )
-    );
-});
